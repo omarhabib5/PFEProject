6801b16940840bb63f659f65d9fcd63d364c93b4
omarhabib5 <oh8852237@gmail.com>
2026-02-11 08:34:44 +0100

    Update app routes configuration

--- PFEProject-omar2/Frontend/webApp/src/app/app.routes.ts
+++ PFEProject-omar2/Frontend/webApp/src/app/app.routes.ts
@@ -9,20 +9,36 @@ import { ServicePage } from './Component/Page/service-page/service-page';
 import { ProjectView } from './Component/Page/Projet/project-view/project-view';
 import { SprintView } from './Component/Page/Sprint/sprint-view/sprint-view';
 import { TeamView } from './Component/Page/Team/team-view/team-view';
+import { TaskView } from './Component/Page/Task/task-view/task-view';
+import { UserStoryView } from './Component/Page/UserStory/user-story-view/user-story-view';
+import { ProjectManager } from './Component/Page/Projet/project-manager/project-manager';
+import { SprintManager } from './Component/Page/Sprint/sprint-manager/sprint-manager';
+import { TeamManage } from './Component/Page/Team/team-manage/team-manage';
+import { TaskManager } from './Component/Page/Task/task-manager/task-manager';
+import { UserStoryManager } from './Component/Page/UserStory/user-story-manager/user-story-manager';
 export const routes: Routes = [
 
     {path:'',component:Login },
-    {path:'AdminDashboard', component:AdminDashboard , 
-        children:[
-        {path:'users', component:UserManage},
-        {path:'Service', component:ServicePage , },
-        {path:'ProjectView',component:ProjectView},
-        {path:'SprintView',component:SprintView},
-        {path:'TeamView',component:TeamView},
-        {path:'TaskView'}
+    {path:'AdminDashboard', component:AdminDashboard},
+    
+    {path:'users', component:UserManage},
+    {path:'Service', component:ServicePage , },
+    
+    {path:'ProjectView',component:ProjectView},
+    {path:'SprintView',component:SprintView},
+    {path:'TeamView',component:TeamView},
+    {path:'TaskView',component:TaskView},
+    {path:'UserStoryView',component:UserStoryView},
+
+
+    {path:'ProjectManage',component:ProjectManager},
+    {path:'SprintManage',component:SprintManager},
+    {path:'TeamManage',component:TeamManage},
+    {path:'TaskManage',component:TaskManager},
+    {path:'UserStoryManage',component:UserStoryManager},
         
 
-        ]},
+
     {path:'ChefProjetDashboard', component:ChefProjetDashboard},
     {path:'EmployeeDashboard', component:EmployeeDashboard},
     {path:'ResponsableServiceDashboard', component:ResponsableServiceDashboard},
